c0c37dfc30fc6ff63678001febb74684612a09cf
Ramazan Kacmaz <98986481+RamazanKacmaz@users.noreply.github.com>
2023-09-27 08:31:30 +0200

    27.09.2023

--- src/test/java/day01_driverMethodlar/C01_IlkClass.java
+++ src/test/java/day01_driverMethodlar/C01_IlkClass.java
@@ -1,7 +1,9 @@
+package day01_driverMethodlar;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class C01_ilktest {
+public class C01_IlkClass {
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
